cb2c5e758d2b37030b4084f963340d51d9010048
Gloria <demouser@MacBook-Pro.local>
2017-07-18 00:45:41 -0400

    first draft

--- index.js
+++ index.js
@@ -2,12 +2,22 @@ var BR = "<br />";
 
 function setup(){
   $("#submitButton").click(postComment);
+  $("#submitButton").mouseenter(addBorder);
+  $("#submitButton").mouseleave(removeBorder);
+}
+
+function addBorder(){
+  $(this).addClass("hover");
+}
+function removeBorder(){
+  $(this).removeClass("hover");
 }
 
 function postComment(event){
   comment = $("#commentArea").val();
-  $("#displayComments").append(comment+BR)
+  $("#commentArea").val("");
+  $("#displayComments").append(comment+BR);
 
   //selct div, append into p
 }
-$('document').ready(setup)
+$(document).ready(setup);
